d7158678a2f759a995db99cd3cbb4fc33fbf6394
saifur550 <srstap550@gmail.com>
2021-09-18 06:11:35 +0600

    1st react apps

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+// import logo from './physics.png';
+import { createPortal } from 'react-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Person name="sakib al hasan1" team="BD1"></Person>
+     <Person name="androw" team="DD2"></Person>
+     <Person name="rassel" team="BBD3"></Person>
+     <Person name="lass" team="BCD4"></Person>
+     <Person name="sakib al hasan5" team="BFFD5"></Person>
+     <Person name="sakib al hasan6" team="BDCD6"></Person>
+     <Userinfo></Userinfo>
+    </div>
+  );
+}
+
+function Person(props){
+  const person = {
+    backgroundColor: 'coral',
+    border: '2px solid gray',
+    margin:'20px',
+    borderRadius: '20px',
+   
+  }
+  return (
+    <div style={person}>
+    <h1> {props.name}</h1>
+    <h4>country :{props.team}</h4>
+  </div>
+  );
+}
+
+
+function Userinfo(){
+  const bg = {
+    backgroundColor:'skyblue',
+    height: '200px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    borderRadius: '50px',
+    margin:'20px',
+
+  }
+  return(
+    <div style={bg}>
+      <h3>player info</h3>
+      <h4>Age:32</h4>
     </div>
   );
 }
